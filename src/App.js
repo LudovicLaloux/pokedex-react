f44ba132ab6ddfba31c73a1429f21974ec40c6ec
LudoL <ludo.laloux@gmail.com>
2019-11-16 12:51:24 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {render} from 'react-dom';
+import React, {Component} from 'react';
+import fetch from 'isomorphic-fetch';
+import PokemonList from './components/PokemonList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//This is the root component
+class PokeApp extends Component{
+  render(){
+    return <div className="pokeapp">
+      <h1> The Kanto PokeDex! </h1>
+      <PokemonList/>
+    </div>;
+  }
 }
 
-export default App;
+export default PokeApp;
